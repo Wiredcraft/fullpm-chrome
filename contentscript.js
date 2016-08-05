25a97b13d0c6bd7f57126d1e75d786c164f6b175
Jamesford <jamiestobbs92@gmail.com>
2016-08-05 15:03:43 +0200

    Set iframe height dynamically on start and resize

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,6 @@
+var iframe
+var contentHeight // header + footer only height
+
 // Append kanban button to page
 function appendButtonToNav () {
   // Stop if button exists already
@@ -15,8 +18,14 @@ function appendButtonToNav () {
   }
 }
 
-var pageHeight = document.documentElement.clientHeight;
+// Resize iframe to fit height
+function resize () {
+  var pageHeight = window.document.documentElement.clientHeight;
+  var iframeHeight = pageHeight - contentHeight - 5; // -5 extra padding/margin
+  iframe.style.cssText = 'width:100%;height:' + iframeHeight  + 'px;border:none';
+}
 
+// Replace content with kanban
 function appendContentToPage () {
   // Get current user/org name & repo name
   var pathArray = window.location.pathname.replace('/', '').split('/')
@@ -27,15 +36,22 @@ function appendContentToPage () {
   // Tab content element
   contentElem = document.getElementsByClassName('repository-content')[0]
 
-  // Create our content
-  var iframe = document.createElement('iframe');
-  iframe.src = chrome.runtime.getURL('frame/index.html' + queryString);
-  iframe.style.cssText = 'width:100%;height:500px;border:none';
-
   // Clear contentElem for our content
   while (contentElem.firstChild) {
     contentElem.removeChild(contentElem.firstChild);
   }
+
+  // Set content height (as only header & footer are left on page now)
+  contentHeight = document.body.clientHeight;
+
+  // Calculate height for iframe
+  var pageHeight = document.documentElement.clientHeight;
+  var iframeHeight = pageHeight - contentHeight - 5; // -5 extra padding/margin
+
+  // Create our content
+  iframe = document.createElement('iframe');
+  iframe.src = chrome.runtime.getURL('frame/index.html' + queryString);
+  iframe.style.cssText = 'width:100%;height:' + iframeHeight + 'px;border:none';
 
   // Append our content to the content element
   contentElem.appendChild(iframe)
@@ -44,12 +60,14 @@ function appendContentToPage () {
   var activeLinkEl = document.getElementsByClassName('selected reponav-item')[0]
   activeLinkEl.classList.remove("selected")
   this.classList.add('selected')
+
+  window.onresize = resize
 }
 
 // Background script message listener
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   switch (request.type) {
-    case 'complete':
+    case 'complete': // GitHub AJAX page updates complete, ensure kanban button is still appended to tabs
       appendButtonToNav()
       break
     default:
--- frame/script.js
+++ frame/script.js
@@ -14,7 +14,7 @@ function generateIframe(queryObj) {
 
   var iframe = document.createElement('iframe');
   iframe.src = 'https://staging-kenhq.wiredcraft.net/boards/' + queryObj.name + '/' + queryObj.repo;
-  iframe.style.cssText = 'width:100%;height:400px;border:none';
+  iframe.style.cssText = 'width:100%;height:100%;border:none';
 
   return iframe
 };
@@ -22,5 +22,3 @@ function generateIframe(queryObj) {
 var iframe = generateIframe(getJsonFromUrl())
 var kanban = document.getElementById('kanban')
 kanban.appendChild(iframe)
-
-console.info('Iframe injected for:', iframe.src)
